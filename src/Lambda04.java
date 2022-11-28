c0845fb53e7d7e9ba222bf844988c4717f6854a3
Balcihatice <aciceaybike@gmail.com>
2022-11-29 00:26:28 +0300

    LambdaLesson

--- src/Lambda04.java
+++ src/Lambda04.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lambda04 {
 
@@ -16,31 +18,46 @@ fields --> Universite (String)
 
     public static void main(String[] args) {
 
-        Universite u01 = new Universite("bogazici","matematik",571,93);
-        Universite u02 = new Universite("istanbul tk","matematik",600,81);
-        Universite u03 = new Universite("istanbul","hukuk",1400,71);
-        Universite u04 = new Universite("marmara","bilg muh",1080,77);
-        Universite u05 = new Universite("odtu","gemi mh",333,74);
+        Universite u01 = new Universite("bogazici", "matematik", 571, 93);
+        Universite u02 = new Universite("istanbul tk", "matematik", 600, 81);
+        Universite u03 = new Universite("istanbul", "hukuk", 1400, 71);
+        Universite u04 = new Universite("marmara", "bilg muh", 1080, 77);
+        Universite u05 = new Universite("odtu", "gemi mh", 333, 74);
 
-       List<Universite> unv = new ArrayList<>(Arrays.asList(u01,u02,u03,u04,u05));
+        List<Universite> unv = new ArrayList<>(Arrays.asList(u01, u02, u03, u04, u05));
 
         System.out.println(notOrtalamasi74NuyukUnv(unv));
-
+        System.out.println(matBolumVarMi(unv));
+        System.out.println(ogrSayiBuyuktenKucuge(unv));
 
     }
 
-     //task 01--> butun unversitelerin notOrt'larinin 74' den buyuk oldg kontrol eden pr create ediniz.
+    //task 01--> butun unversitelerin notOrt'larinin 74' den buyuk oldg kontrol eden pr create ediniz.
 
-   public static boolean notOrtalamasi74NuyukUnv(List<Universite> unv){
+    public static boolean notOrtalamasi74NuyukUnv(List<Universite> unv) {
 
-      return  unv.stream().allMatch(t->t.getNotOrt()>74);
-
-
-   }
+        return unv.stream().//akisi sagladik  universiteler u01,u02,u03,u04,u05
+                allMatch(t -> t.getNotOrt() > 74);
 
 
+    }
+    //task 02-->universite'lerde herhangi birinde "matematik" olup olmadigini  kontrol eden pr create ediniz.
+
+    public static boolean matBolumVarMi(List<Universite> unv) {
+        return unv.
+                stream().//akis saglandi
+                        anyMatch(t -> t.getBolum(). //objlerin bolum isimleri alindi
+                        toLowerCase().//bolum isimleri kucuk harfe cevrildi
+                        contains("mat")); //mat kelimesi varmi kontrol
+    }
 
 
+    //task 03-->universite'leri ogr sayilarina gore b->k siralayiniz.
+    public static List<Universite> ogrSayiBuyuktenKucuge(List<Universite> unv) {
+       return unv.stream().//akis saglandi
+                sorted(Comparator.comparing(Universite::getOgrSayisi).reversed()).
+                //universiteler ogrenci sayisina gore tersten siralandi
+                collect(Collectors.toList());//yukardan gelen stream yapiyi liste donusturur.
 
-
+    }
 }
